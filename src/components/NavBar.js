7c4e0b11d21b965cd0ea6e568f1230da2b6cff2c
MokshagnaV <mokshagnavemula@gmail.com>
2023-08-18 22:49:31 +0530

    Add sidebar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,88 @@
-import { Link } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Icon,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
 import "./navbar.css";
+import { useState } from "react";
+import { Close, Menu } from "@mui/icons-material";
 
 const NavBar = () => {
   const pages = ["Home", "About", "Skills", "Projects", "Contact"];
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const menuClick = () => {
+    const menuStatus = !toggleMenu;
+    setToggleMenu(menuStatus);
+  };
+
   return (
     <div className="nav-bar">
-      <div className="menu">
-        {pages.map((page, index) => (
-          <Link key={index} href={`#${page}`} fontSize={20} underline="none">
-            {page}
-          </Link>
-        ))}
-      </div>
+      <Box display="flex" position={{ xs: "relative", md: "unset" }}>
+        <Box
+          position="absolute"
+          top="8px"
+          left="8px"
+          display={{ xs: "block", md: "none" }}
+        >
+          <IconButton onClick={menuClick}>
+            <Icon fontSize="large">
+              <Menu fontSize="large" onClick={() => setToggleMenu(true)} />
+            </Icon>
+          </IconButton>
+        </Box>
+        <Drawer open={toggleMenu}>
+          <Box padding={1}>
+            <IconButton onClick={menuClick}>
+              <Icon fontSize="large">
+                <Close fontSize="large" onClick={() => setToggleMenu(false)} />
+              </Icon>
+            </IconButton>
+          </Box>
+          <Divider />
+          <List>
+            {pages.map((item, index) => (
+              <Link key={index} href={`#${item}`} underline="none">
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemText primary={item} />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Drawer>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          padding={1}
+          flexGrow={{ xs: 1, md: 0 }}
+        >
+          <Typography
+            variant="h3"
+            fontFamily="'Tilt Prism', cursive"
+            left="16px"
+            sx={{ cursor: "pointer" }}
+          >
+            MV
+          </Typography>
+        </Stack>
+        <Box display={{ xs: "none", md: "flex" }} flexGrow={1} className="menu">
+          {pages.map((page, index) => (
+            <Link key={index} href={`#${page}`} fontSize={20} underline="none">
+              {page}
+            </Link>
+          ))}
+        </Box>
+      </Box>
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,14 @@
 import { Button, Collapse, IconButton, Stack } from "@mui/material";
 import { Close, Menu } from "@mui/icons-material";
-import { useState } from "react";
-const Sidebar = () => {
-  const [toggleMenu, setToggleMenu] = useState(true);
-  const menuClick = () => {
-    const menuStatus = !toggleMenu;
-    setToggleMenu(menuStatus);
-  };
-
+const Sidebar = ({ toggleMenu, menuClick }) => {
   return (
     <Collapse
       in={toggleMenu}
-      sx={{
-        bgcolor: "violet",
-        minHeight: "100vh",
-        height: "inherit",
-        maxWidth: { sm: "30%" },
-      }}
+      // sx={{
+      //   minHeight: "100vh",
+      // }}
       orientation="horizontal"
-      collapsedSize={80}
+      position="absolute"
     >
       <Stack padding={2} alignItems="flex-start">
         <IconButton onClick={menuClick} size="large">
